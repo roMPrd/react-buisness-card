2de5d07c14f0823d956a844841fb3f2fb75f0bad
Romain DELIMAL <rom.delimal@gmail.com>
2023-01-17 21:23:06 +0100

    First Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import PhotoProfile from './images/photoProfile.jpg';
 import './App.css';
+import Main from './components/Main'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main--container">
+        <img id='profile-photo' src={PhotoProfile} alt='profile' />
+      </div>
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import './Main.css'
+
+function Main () {
+  return (
+    <>
+      <div className='title--bio'>
+
+      </div>
+    </>
+  )
+}
+
+export default Main;
